84b4cb3070d63706183fe7e4601af304e13289e9
Nishant Kumar <nishantk071998@gmail.com>
2024-03-09 19:18:02 +0530

    removing

--- src/jsx/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Home =(props)=>{
-    console.log(props.userInfo)
-    return(
-    <div>
-       <label>WELCOME TO HOME PAGE {props.userInfo.userName}</label> 
-    </div>)
-}
